c955060d49494fe8956557c46bb6143b40f7a7b6
ChristalCamillo <christal.b.camillo@gmail.com>
2024-03-05 17:05:47 -0300

    adicionando o jest

--- index.js
+++ index.js
@@ -1,40 +1,5 @@
-import assert from 'node:assert/strict';
-
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-// usaremos asserções como forma de comparar true ou false.
-const verifiqueSe = (valor) => {
-  // vamos guardar todos os testes aqui
-  const assercoes = {
-    ehExatamenteIgualA(esperado) {
-      assert.strictEqual(valor, esperado);
-    },
-  };
-  return assercoes;
-};
-
-// faremos uma func de teste mais generica
-const teste = (titulo, funcaoDeTeste) => {
-  try {
-    funcaoDeTeste();
-    console.log(`${titulo} passou`);
-  } catch {
-    console.error(`${titulo} nao passou`);
-  }
-};
-
-teste('somaHorasExtras', () => {
-  const valorEsperado = 2500;
-  const valorRetornado = somaHorasExtras(2000, 500);
-
-  verifiqueSe(valorRetornado).ehExatamenteIgualA(valorEsperado);
-});
-
-teste('calculaDesconto', () => {
-  const valorEsperado = 2300;
-  const valorRetornado = calculaDescontos(2500, 200);
-
-  verifiqueSe(valorRetornado).ehExatamenteIgualA(valorEsperado);
-});
+export { somaHorasExtras, calculaDescontos };
